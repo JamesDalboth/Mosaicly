1df1a9fbbc9795ab876953c79a3b153b6702e6e5
Harry <no>
2018-02-24 13:49:08 +0000

    ColourVal class largely completed

--- src/utils/ColourVal.java
+++ src/utils/ColourVal.java
@@ -1,16 +1,22 @@
 package utils;
 
+import picture.Color;
+
 public class ColourVal {
-    final Colour avgCol;
+    final Color avgCol;
     final SearchColour searchByColour;
 
-    public ColourVal(Colour avgCol){
+    public ColourVal(Color avgCol){
         this.avgCol = avgCol;
         this.searchByColour = searchByRepresentation();
     }
     private SearchColour searchByRepresentation(){
         //Will take avgCol and determine which colour bucket it fits into for search purposes
         //Is dependent on implementation of colour
+        static double
+        static double absBlue = ;
+
+
         return SearchColour.BLUE;
         //Placeholder return before implementation of colour is seen
     }
