19a15a7f14a7327dfd38ecc02867ba2b31fabe02
rohit <rohit11may@gmail.com>
2018-02-24 13:09:07 +0000

    Added stitcher class. Added Location class.

---
+++ src/utils/Location.java
@@ -0,0 +1,29 @@
+package utils;
+
+public class Location {
+  // 0,0 is top left.
+
+  private int x;
+  private int y;
+
+  public int getX() {
+    return x;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + x + "," + y + ")";
+  }
+}
