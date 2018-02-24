8eb0f5614816f9d1d4fb68bb462a737fe3572e2d
rohit <rohit11may@gmail.com>
2018-02-24 23:09:27 +0000

    Finished colour classifier.

--- src/AIStuff/ColourClassifier.java
+++ src/AIStuff/ColourClassifier.java
@@ -7,33 +7,104 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import picture.Color;
 import weka.classifiers.Classifier;
-import weka.classifiers.Evaluation;
-import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.bayes.NaiveBayesMultinomial;
-import weka.classifiers.bayes.NaiveBayesUpdateable;
-import weka.classifiers.functions.LinearRegression;
-import weka.classifiers.functions.MultilayerPerceptron;
-import weka.classifiers.lazy.IBk;
-import weka.classifiers.lazy.KStar;
-import weka.classifiers.meta.ClassificationViaRegression;
-import weka.classifiers.pmml.consumer.NeuralNetwork;
-import weka.classifiers.trees.RandomForest;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
-import weka.core.neighboursearch.BallTree;
 
 public class ColourClassifier {
 
-  public static void main(String[] args) throws Exception {
+  private Classifier classifier;
 
+  public ColourClassifier() {
+    try {
+      this.classifier = loadModel(new File("weka_models"), "/RandomForest");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+
+    ColourClassifier c = new ColourClassifier();
+    System.out.println("Loaded model.");
+    System.out.println(c.classify(new Color(0, 0, 0)));
+  }
+
+  public static Classifier loadModel(File path, String name) throws Exception {
+
+    Classifier classifier;
+
+    FileInputStream fis = new FileInputStream(path + name + ".model");
+    ObjectInputStream ois = new ObjectInputStream(fis);
+
+    classifier = (Classifier) ois.readObject();
+    ois.close();
+
+    return classifier;
+  }
+//
+//    List<String> readAllLines = Files.readAllLines(Paths.get("data.txt"));
+//    for (int i = 0; i < readAllLines.size(); i++) {
+////      if (i % 10000 == 0){
+////        System.out.println("i = " + i);
+////      }
+//      String line = readAllLines.get(i);
+//
+//      String[] splitted = line.split(" ");
+//      int red = Integer
+//          .parseInt(splitted[0].substring(1, splitted[0].length() - 1));
+//      int green = Integer
+//          .parseInt(splitted[1].substring(0, splitted[1].length() - 1));
+//      int blue = Integer
+//          .parseInt(splitted[2].substring(0, splitted[2].length() - 1));
+//      String colour = splitted[3].substring(0, splitted[3].length())
+//          .toUpperCase();
+//      if (splitted.length == 5) {
+//        colour = splitted[4].toUpperCase();
+//      }
+//      Instance next = new DenseInstance(4);
+//      next.setValue(R, red);
+//      next.setValue(G, green);
+//      next.setValue(B, blue);
+//      next.setValue(COLOUR, colour);
+//      isTrainingSet.add(next);
+//    }
+////    Classifier cModel = new RandomForest();
+////    cModel.buildClassifier(isTrainingSet);
+////    System.out.println("Built the classifier!");
+////    saveModel(cModel, "RandomForest");
+//
+//
+//    // Test the model
+////    Evaluation eTest = new Evaluation(isTrainingSet);
+////    eTest.evaluateModel(cModel, isTrainingSet);
+////
+////    // Print the result à la Weka explorer:
+////    String strSummary = eTest.toSummaryString();
+////    System.out.println(strSummary);
+
+  private static void saveModel(Classifier c, String name) throws Exception {
+    ObjectOutputStream oos = null;
+    try {
+      oos = new ObjectOutputStream(
+          new FileOutputStream("weka_models" + name + ".model"));
+
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    } catch (IOException e1) {
+      e1.printStackTrace();
+    }
+    oos.writeObject(c);
+    oos.flush();
+    oos.close();
+  }
+
+  public Instances getTestSet() {
     //1.ATTRIBUTES
 
     // Declare two numeric attributes
@@ -72,81 +143,28 @@ public class ColourClassifier {
     wekaAttributes.add(COLOUR);
 
     // Create an empty training set
-    Instances isTrainingSet = new Instances("Rel", wekaAttributes, 200000);
+    Instances isTrainingSet = new Instances("Rel", wekaAttributes, 1);
 
     // Set class index
     isTrainingSet.setClassIndex(3);
-
-    List<String> readAllLines = Files.readAllLines(Paths.get("data.txt"));
-    for (int i = 0; i < readAllLines.size(); i++) {
-//      if (i % 10000 == 0){
-//        System.out.println("i = " + i);
-//      }
-      String line = readAllLines.get(i);
-
-      String[] splitted = line.split(" ");
-      int red = Integer
-          .parseInt(splitted[0].substring(1, splitted[0].length() - 1));
-      int green = Integer
-          .parseInt(splitted[1].substring(0, splitted[1].length() - 1));
-      int blue = Integer
-          .parseInt(splitted[2].substring(0, splitted[2].length() - 1));
-      String colour = splitted[3].substring(0, splitted[3].length())
-          .toUpperCase();
-      if (splitted.length == 5) {
-        colour = splitted[4].toUpperCase();
-      }
-      Instance next = new DenseInstance(4);
-      next.setValue(R, red);
-      next.setValue(G, green);
-      next.setValue(B, blue);
-      next.setValue(COLOUR, colour);
-      isTrainingSet.add(next);
-    }
-//    Classifier cModel = new RandomForest();
-//    cModel.buildClassifier(isTrainingSet);
-//    System.out.println("Built the classifier!");
-//    saveModel(cModel, "RandomForest");
-
-    Classifier cModel = loadModel(new File("weka_models"), "/RandomForest");
-    System.out.println("Loaded model.");
-    // Test the model
-    Evaluation eTest = new Evaluation(isTrainingSet);
-    eTest.evaluateModel(cModel, isTrainingSet);
-
-    // Print the result à la Weka explorer:
-    String strSummary = eTest.toSummaryString();
-    System.out.println(strSummary);
-
+    return isTrainingSet;
   }
 
-  private static void saveModel(Classifier c, String name) throws Exception {
-    ObjectOutputStream oos = null;
+  public double classify(Color color) {
+    // Create an empty test set
+    Instances testSet = getTestSet();
+    // Set class index
+
+    Instance instance = new DenseInstance(3);
+    instance.setValue(0, color.getRed());
+    instance.setValue(1, color.getBlue());
+    instance.setValue(2, color.getGreen());
+    testSet.add(instance);
     try {
-      oos = new ObjectOutputStream(
-          new FileOutputStream("weka_models" + name + ".model"));
-
-    } catch (FileNotFoundException e1) {
-      e1.printStackTrace();
-    } catch (IOException e1) {
-      e1.printStackTrace();
+      return classifier.classifyInstance(testSet.instance(0));
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    oos.writeObject(c);
-    oos.flush();
-    oos.close();
-  }
-
-
-  private static Classifier loadModel(File path, String name) throws Exception {
-
-    Classifier classifier;
-
-    FileInputStream fis = new FileInputStream(path + name + ".model");
-    ObjectInputStream ois = new ObjectInputStream(fis);
-
-    classifier = (Classifier) ois.readObject();
-    ois.close();
-
-    return classifier;
+    return 0;
   }
 }
