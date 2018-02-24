0dde8a4e443622cb04502f03fbcf26449446ade3
Harry <no>
2018-02-24 13:34:39 +0000

    ColourVal class largely completed

--- src/utils/ColourVal.java
+++ src/utils/ColourVal.java
@@ -4,9 +4,15 @@ public class ColourVal {
     final Colour avgCol;
     final SearchColour searchByColour;
 
-    public ColourVal(Colour avgCol, SearchColour searchByColour){
+    public ColourVal(Colour avgCol){
         this.avgCol = avgCol;
-        this.searchByColour = searchByColour;
+        this.searchByColour = searchByRepresentation();
+    }
+    private SearchColour searchByRepresentation(){
+        //Will take avgCol and determine which colour bucket it fits into for search purposes
+        //Is dependent on implementation of colour
+        return SearchColour.BLUE;
+        //Placeholder return before implementation of colour is seen
     }
     public enum SearchColour{
         RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, PINK, WHITE, GREY, BLACK, BROWN;
